9004e7bd04e4d66bc110608bfdad1dc87c8d461f
Elias Mawa <elias@emawa.io>
2023-04-07 21:32:08 -0600

    feat: cms skeleton

---
+++ packages/cms/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: false,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  ignorePatterns: ["playwright.config.ts"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "error",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
